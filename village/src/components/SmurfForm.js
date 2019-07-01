cb49f40e0c8f7b4c9ec38c97974f4431d15ae6d7
Zac Smith <zrs3141592@gmail.com>
2019-07-01 12:49:04 -0600

    axios post on form page

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    const { name, age, height } = this.state;
+
+    axios.post("http://localhost:3333/smurfs", {
+      name: name,
+      age: age,
+      height: height
+    });
 
     this.setState({
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     });
-  }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
