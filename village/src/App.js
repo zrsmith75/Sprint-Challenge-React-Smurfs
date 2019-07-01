cdbdb0f564b6beb5e60df8a0ab9b8804b377f420
Zac Smith <zrs3141592@gmail.com>
2019-07-01 13:02:57 -0600

    displaying new smurf entries

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 import SmurfForm from "./components/SmurfForm";
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-
+import { Route, Link } from "react-router-dom";
 const Smurf = props => {
   return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
+    <React.Fragment>
+      <Route path="/" exact>
+        <div className="Smurf">
+          <h3>{props.name}</h3>
+          <strong>{props.height} tall</strong>
+          <p>{props.age} smurf years old</p>
+        </div>
+      </Route>
+    </React.Fragment>
   );
 };
 
